5f2f7bfe05b1c4518f4d5ab19af957993d2407f0
Zeynep Kübra Batmaz <z.k.batmaz@gmail.com>
2025-05-19 15:41:53 +0300

    ReadME

--- TranscriptionBackend/obj/Debug/net8.0/TranscriptionBackend.AssemblyInfo.cs
+++ TranscriptionBackend/obj/Debug/net8.0/TranscriptionBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TranscriptionBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5766ef52d3d998ba8310776c094aa13cc282cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34280233d3bd2b1ca5869d78a9c0b2c70493abd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TranscriptionBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TranscriptionBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
